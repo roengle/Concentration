cc6aad8026ba9738418c6101f010b363156f5d3f
KShut <kennethshuto@gmail.com>
2021-04-27 00:07:00 -0700

    Layout v0.1
    > Changed layout of the Main Screen and added a Play Screen
    > Added a spinner that can select from 2-20 cards.
    > Added buttons and TextViews for layout purposes only

--- app/src/main/java/cs2450/swingsharp/MainActivity.java
+++ app/src/main/java/cs2450/swingsharp/MainActivity.java
@@ -3,6 +3,10 @@ package cs2450.swingsharp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Spinner code
+        String[] arraySpinner = new String[] {
+                "2 Cards", "4 Cards", "6 Cards", "8 Cards", "10 Cards",
+                "12 Cards", "14 Cards", "16 Cards", "18 Cards", "20 Cards"};
+        Spinner s = (Spinner) findViewById(R.id.difficultySpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
     }
 }
