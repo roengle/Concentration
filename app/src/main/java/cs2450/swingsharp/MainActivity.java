5d0a0c60788160d777f081df377fea1f9dce2182
Robert Engle <robertibengle@gmail.com>
2021-04-26 18:40:21 -0700

    first commit

---
+++ app/src/main/java/cs2450/swingsharp/MainActivity.java
@@ -0,0 +1,14 @@
+package cs2450.swingsharp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
