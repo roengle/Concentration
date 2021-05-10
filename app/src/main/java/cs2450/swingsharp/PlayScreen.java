43a79d5b0edabbc4dd8a5cba202004980e2d0bba
Saiph0 <72117903+Saiph0@users.noreply.github.com>
2021-05-10 05:55:16 -0700

    Only 2 similar cards allowed on field
    Todo:
    > Create method for making a "Card" instead of buttons
    > Change card height and width dynamically
    > Adjust layout for rotation

--- app/src/main/java/cs2450/swingsharp/PlayScreen.java
+++ app/src/main/java/cs2450/swingsharp/PlayScreen.java
@@ -61,41 +61,68 @@ public class PlayScreen extends AppCompatActivity {
 
         //Creates an array of words used in the current game
         Random r = new Random();
-        String[] chosenWords = new String[difficulty/2];
+        String[] chosenWords = new String[difficulty];
         for (int i = 0; i < difficulty / 2; i++) {  //Ex: if 14 cards, pick out 7 names
             chosenWords[i] = wordBank[i];
+            chosenWords[i+(difficulty/2)] = wordBank[i];
         }
 
         //Creates an X amount of cards based on the array of words (max of 5rows x 4col)
+        int tempNum;
         for(int i = 1; i <= difficulty; i++){
             if(i >= 1 && i <= 4) {
                 Button button = new Button(this);
-                String word = chosenWords[r.nextInt(chosenWords.length)];   //(Change later) picks a random word from the list
+                String word=null;
+                while(word==null){
+                    tempNum = r.nextInt(chosenWords.length);
+                    word = chosenWords[tempNum];
+                    chosenWords[tempNum] = null;
+                }
                 button.setText(word);
                 button.setHeight(100);
                 tablerow1.addView(button, tlp);
             }
             if(i >= 5 && i <= 8){
                 Button button = new Button(this);
-                String word = chosenWords[r.nextInt(chosenWords.length)];   //(Change later) picks a random word from the list
+                String word=null;
+                while(word==null){
+                    tempNum = r.nextInt(chosenWords.length);
+                    word = chosenWords[tempNum];
+                    chosenWords[tempNum] = null;
+                }
                 button.setText(word);
                 tablerow2.addView(button, tlp);
             }
             if(i >= 9 && i <= 12){
                 Button button = new Button(this);
-                String word = chosenWords[r.nextInt(chosenWords.length)];   //(Change later) picks a random word from the list
+                String word=null;
+                while(word==null){
+                    tempNum = r.nextInt(chosenWords.length);
+                    word = chosenWords[tempNum];
+                    chosenWords[tempNum] = null;
+                }
                 button.setText(word);
                 tablerow3.addView(button, tlp);
             }
             if(i >= 13 && i <= 16){
                 Button button = new Button(this);
-                String word = chosenWords[r.nextInt(chosenWords.length)];   //(Change later) picks a random word from the list
+                String word=null;
+                while(word==null){
+                    tempNum = r.nextInt(chosenWords.length);
+                    word = chosenWords[tempNum];
+                    chosenWords[tempNum] = null;
+                }
                 button.setText(word);
                 tablerow4.addView(button, tlp);
             }
             if(i >= 17){
                 Button button = new Button(this);
-                String word = chosenWords[r.nextInt(chosenWords.length)];   //(Change later) picks a random word from the list
+                String word=null;
+                while(word==null){
+                    tempNum = r.nextInt(chosenWords.length);
+                    word = chosenWords[tempNum];
+                    chosenWords[tempNum] = null;
+                }
                 button.setText(word);
                 tablerow5.addView(button, tlp);
             }
